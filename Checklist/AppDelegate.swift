84b6dcecdade64a2a3604009120b8c7a3a84d20c
animevn <haanhth21@gmail.com>
2019-09-15 23:42:45 +0700

    check ok now

--- Checklist/AppDelegate.swift
+++ Checklist/AppDelegate.swift
@@ -7,22 +7,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     let dataModel = DataModel()
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions:[UIApplication.LaunchOptionsKey: Any]?)->Bool{
         
         window?.backgroundColor = .orange
         let navigation = window!.rootViewController as! UINavigationController
         let controller = navigation.viewControllers[0] as! AllListsVC
         controller.dataModel = dataModel
         
-        let notificationCenter = UNUserNotificationCenter.current()
-        notificationCenter.requestAuthorization(options: [.alert, .sound]){
-            granted, error in
-            if granted{
-                print("granted")
-            }else{
-                print("denied")
-            }
-        }
+//        let notificationCenter = UNUserNotificationCenter.current()
+//        notificationCenter.requestAuthorization(options: [.alert, .sound]){
+//            granted, error in
+//            if granted{
+//                print("granted")
+//            }else{
+//                print("denied")
+//            }
+//        }
         
 //        let content = UNMutableNotificationContent()
 //        content.title = "Hello!"
@@ -39,6 +41,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
 
     }
+    
+    private func save(){
+        dataModel.saveChecklist()
+    }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         save()
@@ -52,9 +58,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     }
 
-    private func save(){
-        dataModel.saveChecklist()
-    }
+   
     func applicationWillTerminate(_ application: UIApplication) {
         save()
     }
