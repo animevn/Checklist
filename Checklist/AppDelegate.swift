fce3503b2f062c02c398adc03727ae434d47e683
animevn <haanhth21@gmail.com>
2020-02-08 09:33:19 +0700

    reset

--- Checklist/AppDelegate.swift
+++ Checklist/AppDelegate.swift
@@ -5,36 +5,12 @@ import UserNotifications
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    let dataModel = DataModel()
 
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions:[UIApplication.LaunchOptionsKey: Any]?)->Bool{
         
         window?.backgroundColor = .orange
-        let navigation = window!.rootViewController as! UINavigationController
-        let controller = navigation.viewControllers[0] as! AllListsVC
-        controller.dataModel = dataModel
-        
-        let notificationCenter = UNUserNotificationCenter.current()
-        notificationCenter.requestAuthorization(options: [.alert, .sound]){
-            granted, error in
-            if granted{
-                print("granted")
-            }else{
-                print("denied")
-            }
-        }
-        
-        let content = UNMutableNotificationContent()
-        content.title = "Hello!"
-        content.body = "Local Notification"
-        content.sound = UNNotificationSound.default
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
-        let request = UNNotificationRequest(identifier: "MyNotification",
-                                            content: content,
-                                            trigger: trigger)
-        notificationCenter.add(request)
         return true
     }
 
@@ -42,12 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     }
     
-    private func save(){
-        dataModel.saveChecklist()
-    }
-
     func applicationDidEnterBackground(_ application: UIApplication) {
-        save()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -60,7 +31,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
    
     func applicationWillTerminate(_ application: UIApplication) {
-        save()
     }
 }
 
